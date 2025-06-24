4b63291738cf1dadd8b2d78aa7db09462ceac496
Mikhail Rodionov <misha25025.su@gmail.com>
2025-06-24 23:25:39 +0300

    added fsd architecture based structure

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,4 +1,8 @@
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
+import {
+	LOCAL_STORAGE_THEME_KEY,
+	Theme,
+	ThemeContext,
+} from "../lib/ThemeContext";
 import { FC, useMemo, useState } from "react";
 
 const defaultTheme =
