965c8663aa85f66b38143a667fbe6003177c19e2
Mikhail Rodionov <misha25025.su@gmail.com>
2025-06-26 21:51:25 +0300

    added: Navbar, link ui kit component

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,15 +1,13 @@
-import { Simulate } from "react-dom/test-utils";
-
 type Mods = Record<string, boolean | string>;
 
 export function classNames(
 	cls: string,
-	mods: Mods,
-	additional: string[],
+	mods: Mods = {},
+	additional: string[] = [],
 ): string {
 	return [
 		cls,
-		...additional,
+		...additional.filter(Boolean),
 		...Object.entries(mods)
 			.filter(([_, value]) => Boolean(value))
 			.map(([className]) => className),
