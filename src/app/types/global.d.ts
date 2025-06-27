2ec60c87c2272b11845919bfe52e1b60a1d0a6fb
Mikhail Rodionov <misha25025.su@gmail.com>
2025-06-27 22:53:36 +0300

    added: i18n, LangSwitcher widget, Define plugin

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -15,7 +15,5 @@ declare module "*.svg" {
 	const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
 	export default SVG;
 }
-// declare module "*.svg" {
-// 	const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-// 	export default content;
-// }
+
+declare const __IS_DEV__: boolean;
