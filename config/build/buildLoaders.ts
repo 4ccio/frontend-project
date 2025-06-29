91cd4ebfa2e0cb90ae6bdcc2eaafafb4ef915566
Mikhail Rodionov <misha25025.su@gmail.com>
2025-06-29 21:37:12 +0300

    added: eslint, fixed eslint errors

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,32 +1,32 @@
-import webpack from "webpack";
-import { BuildOptions } from "./types/config";
-import { buildBabelLoader } from "./loaders/buildBabelLoader";
-import { buildCssLoader } from "./loaders/buildCssLoader";
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
+import { buildCssLoader } from './loaders/buildCssLoader';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
-	const typeScriptLoader = {
-		test: /\.tsx?$/,
-		use: "ts-loader",
-		exclude: /node_modules/,
-	};
+    const typeScriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    };
 
-	const CSSLoader = buildCssLoader(options);
+    const CSSLoader = buildCssLoader(options);
 
-	const svgLoader = {
-		test: /\.svg$/,
-		use: ["@svgr/webpack"],
-	};
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    };
 
-	const fileLoader = {
-		test: /\.(png|jpe?g|gif)$/i,
-		use: [
-			{
-				loader: "file-loader",
-			},
-		],
-	};
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    };
 
-	const babelLoader = buildBabelLoader(options);
+    const babelLoader = buildBabelLoader(options);
 
-	return [babelLoader, typeScriptLoader, CSSLoader, svgLoader, fileLoader];
+    return [babelLoader, typeScriptLoader, CSSLoader, svgLoader, fileLoader];
 }
--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -1,19 +1,16 @@
-declare module "*.scss" {
-	interface IClassNames {
-		[className: string]: string;
-	}
+declare module '*.scss' {
+	interface IClassNames {[className: string]: string;}
 	const classNames: IClassNames;
-	export = classNames;
-}
+	export = classNames;}
 
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.jpeg";
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
 
-declare module "*.svg" {
-	import React from "react";
+declare module '*.svg' {
+	import React from 'react';
+
 	const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
-	export default SVG;
-}
+	export default SVG;}
 
 declare const __IS_DEV__: boolean;
