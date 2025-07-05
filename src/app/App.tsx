c71e06b4a3bf451f2887af61c1ae7b497acc1be9
Mikhail Rodionov <misha25025.su@gmail.com>
2025-07-05 15:14:53 +0300

    added: NotFoundPage, Page loader widget and ui loader, translations

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
             <Suspense fallback="">
                 <Navbar />
                 <div className="content-page">
-                    <Sidebar className="app" />
+                    <Sidebar />
                     <AppRouter />
                 </div>
             </Suspense>
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
-    return <div style={{ color: 'green' }}>{t('Главная страница')}</div>;
+    return <div>{t('Главная страница')}</div>;
 };
 
 export default MainPage;
