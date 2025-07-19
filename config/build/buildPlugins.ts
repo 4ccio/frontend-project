57f7cfb0d5187e972cadaaff327895e9b73fdfc7
Mikhail Rodionov <misha25025.su@gmail.com>
2025-07-19 20:40:25 +0300

    edited: removed bundle analyzer from production build

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -21,14 +21,11 @@ export function buildPlugins({
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
     ];
 
     if (isDev) {
         plugins.push(new ReactRefreshWebpackPlugin());
-        // plugins.push(new BundleAnalyzerPlugin());
+        plugins.push(new BundleAnalyzerPlugin());
         // plugins.push(new webpack.HotModuleReplacementPlugin());
     }
 
